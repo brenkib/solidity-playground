3968317bb3c1f8face32bb60767f26ac1e134f65
Bren Brenkib <kennybush96@gmail.com>
2024-07-16 21:57:47 +0200

    External contract usage

---
+++ contracts/Logger.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Logger {
+    mapping(address => uint[]) private payments;
+
+    function log(address _from, uint _amount) public {
+        require(_from != address(0), "Zero address!");
+
+        payments[_from].push(_amount);
+    }
+
+
+    function getEntry(address _from, uint _index) public view returns(uint)  {
+        return payments[_from][_index];
+    }
+    constructor(){
+
+    }
+}
+
+
+contract DemoWithLogger {
+
+    receive() external payable {
+        logger.log(msg.sender, msg.value);
+    }
+
+    Logger internal logger;
+
+    constructor(address _logger) {
+        logger = Logger(_logger);
+    }
+
+    function payment(address _from, uint _number) public view returns(uint)  {
+        return logger.getEntry(_from, _number);
+    }
+}
+
